8b795f805613b2244e53d7afb2aca0fd6453f38f
Jichan <development@jc-lab.net>
2020-08-15 11:13:13 +0900

    fix

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ LOCAL_SOCK="$APPDATA\\gnupg\\S.gpg-agent.extra"
 
 TEMP_PORT=$((31000 + $$ % 1000))
 
-./socat TCP-LISTEN:$TEMP_PORT,bind=127.0.0.1 "UNIX-CONNECT:LOCAL_SOCK" &
+./socat TCP-LISTEN:$TEMP_PORT,bind=127.0.0.1 "UNIX-CONNECT:$LOCAL_SOCK" &
 pid_a=$!
 
 ./putty -R 31000:localhost:$TEMP_PORT
